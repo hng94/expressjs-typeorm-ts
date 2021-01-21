281552433b5afaf3b5f3a5596dde83cd40fa43c6
hoangnguyen94 <nguyenvuhoang1994@gmail.com>
2021-01-21 02:34:00 +0100

    Commit to github

---
+++ src/server.ts
@@ -0,0 +1,52 @@
+import "reflect-metadata";
+import { createConnection } from "typeorm";
+import * as express from "express";
+import * as bodyParser from "body-parser";
+import { Request, Response, NextFunction } from "express";
+import Routes from "./route";
+
+createConnection()
+  .then(async (connection) => {
+    // create express app
+    const app = express();
+    app.use(bodyParser.json());
+
+    // register express routes from defined application routes
+    Routes.forEach((route) => {
+      (app as any)[route.method](
+        route.route,
+        (req: Request, res: Response, next: NextFunction) => {
+          const result = new (route.controller as any)()[route.action](
+            req,
+            res,
+            next
+          );
+          if (result instanceof Promise) {
+            result.then((result) =>
+              result !== null && result !== undefined
+                ? res.send(result)
+                : undefined
+            );
+          } else if (result !== null && result !== undefined) {
+            res.json(result);
+          }
+        }
+      );
+    });
+
+    //Error handler
+    app.use(function (err, req, res, next) {
+      console.error(err.stack);
+      res.status(err.status).send(err.message);
+    });
+    // setup express app here
+    // ...
+
+    // start express server
+    app.listen(3000);
+
+    console.log(
+      "Express server has started on port 3000. Open http://localhost:3000/users to see results"
+    );
+  })
+  .catch((error) => console.log(error));
